1d549bc50a6d82b5ad81ca463627a2bc92661113
Shahrukh <raja.shahrukh@cloudways.com>
2020-10-05 06:28:44 +0000

    First Commit

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/482259.cloudwaysapps.com/rjacyhwvmx/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/482259.cloudwaysapps.com/rjacyhwvmx/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '34ade606880fcde1ef31551a60a1ee38');
+	include_once('/home/482259.cloudwaysapps.com/rjacyhwvmx/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'dBWwJTLGLtah2LX9eYeWJ7K7pLYnv6qa3yT7Xf2V725vRMxc0aeUj4p11VKBaMXn');
+define('SECURE_AUTH_KEY',  'yA4xTFvVISi59SwGDsh1VvY5eSbgK4RdWL02wwD06GV1F7AXAU1vMrL9Qm3XcWSQ');
+define('LOGGED_IN_KEY',    'HH4S38SzqxTNEWgg0KrQIRU43Gh2QtNBp0sMg8GDapbtWAf9or08ST2gDB8TE6xx');
+define('NONCE_KEY',        '8J5q5BST1nsNMW3WcEXyDiBof5HY3bFANVE6Q2Vqur8xjELX78dMAh4aLLvMwRe0');
+define('AUTH_SALT',        'v9yovGvyNfExKXwp5Qqr5JRqzwtp0i5xqTPWSnNbLv2rjmBgoC7QyoB3sEIEEWpI');
+define('SECURE_AUTH_SALT', 'LPv6AgGptPPb8YEWi5wd6tjtKpLRpm4s9h6aqjSuyR5opsjvQYUxA2FUdPjBu1Lu');
+define('LOGGED_IN_SALT',   'WSsyq7sreyudRArjArE806SSyx1rgcUdVpPBp0o6V6s5zTnTVQx840ggum7WssHx');
+define('NONCE_SALT',       'NYx57IPJLPit1HPGt4Cv98pmBeXmUWfic1XBneYzfyjxoxYXAQxRP92Vq8xLTmNs');
